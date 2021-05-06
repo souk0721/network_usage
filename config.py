53a29b7ef5c57f7656bd17b46514ecd50572f3ee
HyunSeok noh <souk0721@gmail.com>
2021-05-06 10:07:04 +0900

    config 수정

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 set_time = 1  #1 = 1Sec
 set_eth_name = "이더넷" #change the inf variable according to the interface
-set_sqldb_name = "network_usage_165" #sqlite3_db_name
+set_sqldb_name = "network_usage_165.db" #sqlite3_db_name
